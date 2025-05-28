4b17ad6364a74ed37ac1a5f3fe1d4f5f31647b6d
siva-wq <vullangulavenkatasivagangadhar@gmail.com>
2025-05-28 09:34:00 +0530

    Update db.php

--- backend/social_api/config/db.php
+++ backend/social_api/config/db.php
@@ -1,14 +1,10 @@
 <?php
 class Database {
-   /* private $host = "sql200.infinityfree.com";
-    private $db_name = "if0_39065318_mansiv";
-    private $username = "if0_39065318";
-    private $password = "2W4mYU5mHHp4Io";*/
-    private $host = "sql310.ezyro.com";
-     private $db_name = "ezyro_39089839_ManSiv";
-     private $username = "ezyro_39089839";
-     private $password = "f3bfb1ea226";
-    public $conn;
+   private $host = "HOST";
+   private $db_name = "DB NAME";
+   private $username = "USERNAME";
+   private $password = "PASSWORD";
+   public $conn;
 
     public function getConnection() {
         $this->conn = null;
